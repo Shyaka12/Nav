8bef5764508a04bade2ea4e1fc0ee619ec9774d3
shyaka12 <shyakap48@gmail.com>
2024-03-16 20:45:21 +0200

    last edit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+function showSideBar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
+
+signinBtn.onclick = function () {
+  nameField.style.maxHeight = "0";
+  title.innerHTML = "Sign In";
+  signupBtn.classList.add("disable");
+  signinBtn.classList.remove("disable");
+}
+
+signupBtn.onclick = function () {
+  nameField.style.maxHeight = "60px";
+  title.innerHTML = "Sign Up";
+  signupBtn.classList.remove("disable");
+  signinBtn.classList.add("disable");
+}
